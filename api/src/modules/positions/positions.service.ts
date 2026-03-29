10cdf181b86a8c5d6908f9b627c180f04aedb8d2
scedze <ada.fomina.00.00@gmail.com>
2026-03-30 02:10:33 +0300

    создание должности, все должности, одна должность по id, обновление давнных и удаление

---
+++ api/src/modules/positions/positions.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, NotFoundException} from '@nestjs/common';
+import {pool} from '../../database/pool';
+@Injectable()
+export class PositionsService {
+    async create(data: {name: string;}) {
+        const query = `
+            INSERT INTO positions(name)
+            VALUES ($1, $2)
+            RETURNING id, name, created_at, updated_at
+        `;
+        const result = await pool.query(query, [data.name]);
+        return result.rows[0];
+    }
+    async findAll() {
+        const query = `
+            SELECT id, name, created_at, updated_at
+            FROM positions
+            WHERE deleted_at IS NULL
+            ORDER BY created_at DESC
+        `;
+        const result = await pool.query(query);
+        return result.rows;
+    }
+    async findOne(id: string) {
+        const query = `
+            SELECT id, name, created_at, updated_at
+            FROM positions
+            WHERE id =$1 AND deleted_at IS NULL
+        `;
+        const result = await pool.query(query, [id]);
+        if (result.rows.length === 0) {
+            throw new NotFoundException(`Positions with ID ${id} not found`);
+        }
+        return result.rows[0];
+    }
+    async update(id: string, data: {name?: string;}) {
+        const updates: string[] = [];
+        const values: any[] = [];
+        let paramIndex = 1;
+        if (data.name !== undefined) {
+            updates.push(`name = $${paramIndex++}`);
+            values.push(data.name);
+        }
+        values.push(id);
+        const query = `
+            UPDATE positions
+            SET ${updates.join(', ')}
+            WHERE id = $${paramIndex} AND deleted_at IS NULL
+            RETURNING id, name, created_at, updated_at
+        `;
+        const result = await pool.query(query, values);
+        if (result.rows.length === 0) {
+            throw new NotFoundException(`Должность с таким iD ${id} не найдена`);
+        }
+        return result.rows[0];
+    }
+    async remove(id: string) {
+        const query = `
+        UPDATE positions
+        SET deleted_at = NOW()
+        WHERE id = $1 AND deleted_at IS NULL
+        RETURNING id
+    `;
+        const result = await pool.query(query, [id]);
+        if (result.rows.length ===0) {
+            throw new NotFoundException(`Должность с таким iD ${id} не найдена`);
+        }
+        return {success: true, message: 'Должность. мягкое удаление'};
+    }
+}
