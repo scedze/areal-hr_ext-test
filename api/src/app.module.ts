34ad08f913845cbe657afe5eb692ae696f5a3619
scedze <ada.fomina.00.00@gmail.com>
2026-03-30 02:23:44 +0300

    обновление

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganizationsModule } from './modules/organizations/organizations.module';
 import { DepartmentsModule } from './modules/departments/departments.module';
+import { PositionsModule } from './modules/positions/positions.module';
 
 @Module({
-  imports: [OrganizationsModule, DepartmentsModule],
+  imports: [OrganizationsModule, DepartmentsModule, PositionsModule],
   controllers: [],
   providers: [],
 })
