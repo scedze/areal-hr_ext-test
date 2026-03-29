ee1617dedb87a28e766f264082e756971b252cd5
scedze <ada.fomina.00.00@gmail.com>
2026-03-30 01:42:53 +0300

    обновление(добавление)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganizationsModule } from './modules/organizations/organizations.module';
+import { DepartmentsModule } from './modules/departments/departments.module';
 
 @Module({
-  imports: [OrganizationsModule],
+  imports: [OrganizationsModule, DepartmentsModule],
   controllers: [],
   providers: [],
 })
